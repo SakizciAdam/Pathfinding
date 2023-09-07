441fc3be6752bf0c33c4a3b84c14903d4ae0c513
SakizciAdam <sakizciadam2000@gmail.com>
2023-09-07 18:03:03 +0300

    SmallPathTest is now compares path result to expected path result

--- Tests/PathFinder_SmallPathTest.cs
+++ Tests/PathFinder_SmallPathTest.cs
@@ -1,3 +1,4 @@
+using Pathfinding.Utils;
 using Xunit;
 
 namespace Pathfinding.Tests;
@@ -26,7 +27,9 @@ public class Pathfinder_SmallPathTest
         */
         var path=p.FindPath();
         Assert.NotNull(path);
-        Assert.True(path.Length==9,"Expected 9 got "+path.Length);
-    
+
+        var d2=PathfindingUtils.ConvertTo2DArray(path);
+        var expected=new int[][]{new int[]{0,0},new int[]{0,1},new int[]{0,2},new int[]{1,2},new int[]{1,3},new int[]{2,3},new int[]{3,3},new int[]{3,2},new int[]{3,1},new int[]{2,1}};
+        Assert.Equal(expected,d2);
     }
 }
