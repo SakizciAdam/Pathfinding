71ed9821582e21550541644a88485f6ed474fd9c
SakizciAdam <sakizciadam2000@gmail.com>
2023-09-07 18:01:53 +0300

    Added utils class to convert tile array to 2d int array

---
+++ Pathfinding/Utils/PathfindingUtils.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pathfinding.Utils {
+    public static class PathfindingUtils {
+        public static int[][] ConvertTo2DArray(Tile[] tiles){
+            int[][] path=new int[tiles.Length][];
+            for(int i=0;i<tiles.Length;i++){
+                path[i]=new int[]{tiles[i].x,tiles[i].y};
+            } 
+            return path;
+        }
+    }
+}
