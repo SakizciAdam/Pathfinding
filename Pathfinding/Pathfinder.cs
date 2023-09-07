9e19c253204fa921d506774922c861d00f380fe0
SakizciAdam <sakizciadam2000@gmail.com>
2023-09-08 01:31:04 +0300

    Visited tiles are now global

--- Pathfinding/Pathfinder.cs
+++ Pathfinding/Pathfinder.cs
@@ -6,6 +6,8 @@ namespace Pathfinding {
         public int startX,startY,endX,endY;
         public readonly int width,height;
 
+        private List<Tile> visited=new List<Tile>();
+
         public Pathfinder(int width,int height,int startX=0,int startY=0,int endX=1,int endY=1){
             this.width=width;
             this.height=height;
@@ -26,6 +28,10 @@ namespace Pathfinding {
             return tile.x==endX&&tile.y==endY;
         }
 
+        public void Clear(){
+            visited.Clear();
+        }
+
         private Tile[] RetracePath(Tile endTile){
             List<Tile> tiles=new List<Tile>();
             Tile current=endTile;
@@ -44,41 +50,25 @@ namespace Pathfinding {
             return tiles.ToArray();
         }
 
-        public void PrintPath(){
-            PrintPath(new List<Tile>());
-        }
-        public void PrintPath(List<Tile> visited){
-            for(int y=0;y<height;y++){
-                for(int x=0;x<width;x++){
-                    char c='.';
-
-                    if(IsVisited(visited,x,y)){
-                        if(!IsValid(x,y)){
-                            c='E';
-                        } else {
-                            c='O';
-                        }
-                    } else if(!IsValid(x,y)){
-                        c='W';
-                    }
-
-                    Console.Write(c);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-
-        public bool IsVisited(List<Tile> visited,int x,int y){
+        public bool IsVisited(int x,int y){
             return visited.FindAll(a => a.x==x&a.y==y).Count>0;
         }
 
-        public bool IsVisited(List<Tile> visited,Tile tile){
-            return visited.FindAll(a => a.x==tile.x&&a.y==tile.y).Count>0;
+        public bool IsVisited(Tile tile){
+            return IsVisited(tile.x,tile.y);
         }
 
+        public bool IsValid(int x,int y){
+            if(x<0||y<0||x>=width||y>=height){
+                return false;
+            }
+
+            return wallLocations.Find(wall => wall.x==x&&wall.y==y)==null;
+        }
+
+        
         public Tile[]? FindPath(){
-            List<Tile> visited=new List<Tile>();
+            
 
             visited.Add(new Tile(startX,startY));
             for(int i=0;i<visited.Count;i++){
@@ -100,12 +90,6 @@ namespace Pathfinding {
 
         }
 
-        public bool IsValid(int x,int y){
-            if(x<0||y<0||x>=this.width||y>=this.height){
-                return false;
-            }
-
-            return wallLocations.Find(wall => wall.x==x&&wall.y==y)==null;
-        }
+        
     }
 }
